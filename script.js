c81307161bf71d7fbe300d3125302730fef749ee
samscarlett <samscarlett07@gmail.com>
2024-03-13 23:35:09 -0400

    new edit

--- script.js
+++ script.js
@@ -6,18 +6,17 @@ let myOutput = WebMidi.outputs[0].channels[1];
 
 let dropIns = document.getElementById("dropdown-ins");
 let dropOuts = document.getElementById("dropdown-outs");
+let interval = document.getElementById("drpodown-interval");
 
-const dropDownOptions = function() {
+let harmony = 0
 
-let maj2nd = document.getElementById("Maj2");
-let maj3rd = document.getElementById("Maj3");
-let perf4 = document.getElementById("Perf4");
-let perf5 = document.getElementById("Perf5");
-let maj6th = document.getElementById("Maj6");
 
-}
+// let maj2nd = document.getElementById("Maj2");
+// let maj3rd = document.getElementById("Maj3");
+// let perf4 = document.getElementById("Perf4");
+// let perf5 = document.getElementById("Perf5");
+// let maj6th = document.getElementById("Maj6");
 
-let dropIntervals = dropDownOptions
 
 //read option n shit
 WebMidi.inputs.forEach(function (input, num) {
@@ -34,11 +33,17 @@ const midiProcess = function (midiNoteInput) {
 
     let pitch = midiNoteInput.note.number;
     let velocity = midiNoteInput.note.rawAttack;
-    let harmPitch = midiNoteInput.note.number + parseInt 
+    // let harmPitch = midiNoteInput.note.number + parseInt 
+
+    let harmony = [note1, note2];
+    let note1 = midiNoteInput.note.number;
+    let note2 = midiNoteInput.note.number + parseInt (interval.value);
 
     let midiNoteOutput = new Note(pitch, { rawAttack: velocity });
-    return midiNoteOutput;
+    return midiNoteOutput, harmony;
 };
+
+
 
 dropIns.addEventListener("change", function () {
     if (myInput.hasListener("noteon")) {
@@ -56,6 +61,33 @@ dropIns.addEventListener("change", function () {
 
     myInput.addListener("noteoff", function (someMIDI) {
         myOutput.sendNoteOff(midiProcess(someMIDI));
+    });
+});
+
+
+
+dropOuts.addEventListener("change", function () {
+    myOutput = WebMidi.outputs [dropOuts.value].channels[1];
+});
+
+//-----------------------------------------------------------------------
+
+dropIns.addEventListener("change", function () {
+    if (mySpecialNote.hasListener("noteon")) {
+    mySpecialNote.removeListener("noteon");
+    }
+    if(mySpecialNote.hasListener("noteoff")) {
+        mySpecialNote.removeListener("noteoff");
+    }
+
+    myInput = WebMidi.inputs[dropIns.value];
+
+    mySpecialNote.addListener("noteon", function (mySpecialNote) {
+        myOutput.sendNoteOn(midiProcess(mySpecialNote));
+});
+
+mySpecialNote.addListener("noteoff", function (mySpecialNote) {
+        myOutput.sendNoteOff(midiProcess(mySpecialNote));
     });
 });
 
